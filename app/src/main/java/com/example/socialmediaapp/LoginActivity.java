ac1ef08e34c043ad1d8ca05700e5b2b8becab90f
Anni1123 <kumarianni1213@gmail.com>
2020-04-14 23:08:16 +0530

    LoginActivity

--- app/src/main/java/com/example/socialmediaapp/LoginActivity.java
+++ app/src/main/java/com/example/socialmediaapp/LoginActivity.java
@@ -1,16 +1,29 @@
 package com.example.socialmediaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText email,password;
-    private Button mRegister;
+    private Button mlogin;
+    private ProgressDialog loadingBar;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +34,60 @@ public class LoginActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         email=findViewById(R.id.emailEtlog);
         password=findViewById(R.id.passwordEtlog);
-        mRegister=findViewById(R.id.loginac);
+        mAuth=FirebaseAuth.getInstance();
+        mlogin=findViewById(R.id.loginac);
+        loadingBar=new ProgressDialog(this);
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowUserToLogin();
+            }
+        });
+
     }
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
     }
+    private void AllowUserToLogin()
+    {
+        String emaill = email.getText().toString();
+        final String pass = password.getText().toString();
+
+        if (TextUtils.isEmpty(emaill))
+        {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(pass))
+        {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Sign In");
+            loadingBar.setMessage("Please wait....");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+            mAuth.signInWithEmailAndPassword(emaill,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+                        Intent mainIntent=new Intent(LoginActivity.this,ProfileActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(mainIntent);
+                        finish();
+                    }
+                    else {
+                        String message=task.getException().toString();
+                        Toast.makeText(LoginActivity.this,"Error:"+message,Toast.LENGTH_LONG).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
+    }
+
+
 }
